e1131b93bafedd5661489913e46d1f7053315916
Moon Arclen <213549704+MoonArclen@users.noreply.github.com>
2026-05-21 14:05:34 -0400

    Add audio functionality with music and sound effects

--- flip-me-not/audio.js
+++ flip-me-not/audio.js
@@ -1,1 +1,7 @@
+let music = new Audio("https://ma.work.gd/sounds/Crimson-Moon");
+let dieSound = new Audio("https://ma.work.gd/sounds/die");
+let musicOn=true, soundOn=true;
 
+function toggleMusic(){musicOn=!musicOn;if(musicOn){music.loop=true;music.play();}else{music.pause();}}
+function toggleSounds(){soundOn=!soundOn;}
+function playDie(){if(soundOn)dieSound.play();}
