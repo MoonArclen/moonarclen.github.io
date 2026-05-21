492fdf3212b50479c3791b9ffbf7a550c7fcdd7c
Moon Arclen <213549704+MoonArclen@users.noreply.github.com>
2026-05-21 14:04:28 -0400

    Add screen management functions to ui.js

--- flip-me-not/ui.js
+++ flip-me-not/ui.js
@@ -1,1 +1,4 @@
-
+const screens=["settingsScreen","shopScreen","howToPlayScreen","creditsScreen","websiteScreen","wikiScreen","leaderboardScreen"];
+function closeAllScreens(){screens.forEach(id=>document.getElementById(id).classList.add("hidden"));}
+function openScreen(screenId){closeAllScreens();document.getElementById(screenId).classList.remove("hidden");}
+function openAccount(){alert("OAuth Coming Soon");}
